79bcaf85736d4a2444642a6604aa67e2ef8636fe
Chihung Yu <chihungyu1116@gmail.com>
2013-07-15 21:52:15 -0700

    done pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,34 +18,38 @@ module ApplicationHelper
 		return score_str
 	end
 
-	def application_get_page_from_merchant(num_merchant_returned)
-
+	def application_get_page_obj_from(count,page_current,page_boundary,page_num_returned)
 		page_obj = {}
-		page_boundary = 10
-		page_current = params[:id] ? params[:id].to_i : nil
-		page_total = (Merchant.count/num_merchant_returned.to_f).ceil
+		page_total = (count/page_num_returned).ceil
+		page_total =8
+		page_boundary_half = (page_boundary/2).ceil
 
 		return redirect_to '/not_found' if page_current.nil? || page_current > page_total
 
-		is_page_current_less_than_5 = page_current < 5
-		is_page_current_plus_5_greater_than_page_total = (page_current + 5) > page_total
+		is_page_current_less_than_or_equal_to_page_boundary_half = page_current <= page_boundary_half
+		is_page_current_plus_page_boundary_half_greater_than_page_total = (page_current + page_boundary_half) > page_total
 
-		if !is_page_current_less_than_5 && !is_page_current_plus_5_greater_than_page_total # page_current = 6, page_total = 100 return [2...11]
-			page_start = page_current - 4
-			page_end = page_current + 5
-		elsif !is_page_current_less_than_5 && is_page_current_plus_5_greater_than_page_total # page_current = 11, page_total = 14 return [5...14]
-			page_start = page_total - 9
+		# based on page_boundary = 9
+		if !is_page_current_less_than_or_equal_to_page_boundary_half && !is_page_current_plus_page_boundary_half_greater_than_page_total # page_current = 6, page_total = 100 return [2...10]
+			page_start = page_current - page_boundary_half
+			page_end = page_current + page_boundary_half
+		elsif !is_page_current_less_than_or_equal_to_page_boundary_half && is_page_current_plus_page_boundary_half_greater_than_page_total # page_current = 11, page_total = 14 return [6...14]
+			page_start = page_total - page_boundary + 1
 			page_end = page_total
-		elsif is_page_current_less_than_5 && !is_page_current_plus_5_greater_than_page_total # page_current = 4, page_total = 14 return [1...10]
+		elsif is_page_current_less_than_or_equal_to_page_boundary_half && !is_page_current_plus_page_boundary_half_greater_than_page_total # page_current = 4, page_total = 14 return [1...9]
 			page_start = 1
-			page_end = 10
+			page_end = page_total
 		else # page_total < page_boundary
 			page_start = 1
-			page_end = page_total
+			page_end = page_boundary
 		end
+
+		page_start = page_start >= 1 ? page_start : 1
+		page_end = page_end <= page_total ? page_end : page_total
 
 		page_obj[:boundaries] = (page_start...page_end+1).to_a
 		page_obj[:current] = page_current
+
 
 		return page_obj
 	end
